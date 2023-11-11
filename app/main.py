13074bf3d3ee056e9fba0a662a3c035b7e3a8162
Paulobs13 <paulo.alessandro05@gmail.com>
2023-11-11 15:38:24 -0300

    update 1

--- app/main.py
+++ app/main.py
@@ -1,24 +1,31 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
-data = read_csv.read_csv('data.csv')
+#data = read_csv.read_csv('data.csv')
 
 country = input('Type Country => ')
 country_name = country
+df = pd.read_csv('data.csv')
 
 if(country == 'World'):
   continent = input('Type Continent: => ')
   continent_name = continent
-  names, values = utils.population_percentage(continent)
-  charts.generate_pie(continent_name, names, values)
+  #names, values = utils.population_percentage(continent)
+
+  df = df[df['Continent'] == continent]
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie(continent_name, countries, percentages)
   print('Chart has been generated succesfully!')
 else:
   result = utils.filter_country(country)
   if len(result) > 0:
-    labels, values = utils.get_population(result)
-    charts.generate_chart (country_name, labels, values)
-    print('Chart has been generated succesfully!')
+   labels, values = utils.get_population(result)
+   
+   charts.generate_chart (country_name, labels, values)
+   print('Chart has been generated succesfully!')
     
 
 
